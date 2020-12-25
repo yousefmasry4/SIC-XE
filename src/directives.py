2ce121392982391d394b36fcc325f1481a03214c
yousseff el masry <youssefmasry04@gmail.com>
2020-12-25 03:43:40 +0200

    word / resw / base

--- src/directives.py
+++ src/directives.py
@@ -1,8 +1,8 @@
-from main import Main
 from .number import Number
 from .models.Lines import Line as l
+from math import ceil
 class Directives:
-    def __init__(self,main:Main):
+    def __init__(self,main):
         self.main=main
         self.linef=lambda c,size : Number(Number(self.main.current_loc).int()+c).hex(size=size)
     
@@ -13,22 +13,51 @@ class Directives:
             self.end(line)
         elif(str(part).upper()) == "BYTE":
             self.byte(line)
+        elif(str(part).upper()) == "WORD":
+            self.word(line)
+        elif(str(part).upper()) == "RESW":
+            self.RESW(line)
+        elif(str(part).upper()) == "BASE":
+            self.BASE(line)
 
 
 
+    def BASE(self,line):
+        if "BASE" != line[0].upper():
+            raise Exception(f"LINE[{ self.main.lineno}]\twrong BASE location")
+        elif len(line) > 2:
+            raise Exception(f"LINE[{ self.main.lineno}]\tWrong BASE formate")  
+        elif  [i.upper() for i in line].count("BASE") > 1:
+            raise Exception(f"LINE[{ self.main.lineno}]\tFalse [Multiple] BASE position")
+        elif line[1].upper() not in self.main.symtab:
+            raise Exception(f"LINE[{ self.main.lineno}]\t {line[1]} not found")      
+        else:
+            self.main.base= self.main.symtab[line[1].upper()]
+            self.main.Lines.append(
+                l(
+                    line,
+                    self.main.lineno,
+                    None,
+                    "BASE",
+                    True
+                )
+            )
 
-    def byte(self,line):
-        if "BYTE" == line[0].upper():
-            raise Exception(f"LINE[{ self.main.lineno}]\tthe name of LABEL can't to set as BYTE")
+
+
+    def RESW(self,line):
+        if "RESW" == line[0].upper():
+            raise Exception(f"LINE[{ self.main.lineno}]\tthe name of LABEL can't to set as RESW")
         elif len(line) > 3:
-            raise Exception(f"LINE[{ self.main.lineno}]\tWrong BYTE formate")  
-        elif  line.count("BYTE") > 1:
-            raise Exception(f"LINE[{ self.main.lineno}]\tFalse [Multiple] BYTE position")
+            raise Exception(f"LINE[{ self.main.lineno}]\tWrong RESW formate")  
+        elif  [i.upper() for i in line].count("RESW") > 1:
+            raise Exception(f"LINE[{ self.main.lineno}]\tFalse [Multiple] RESW position")
         elif line[0].upper() in self.main.symtab:
-            raise Exception(f"LINE[{ self.main.lineno}]\tMULTI useing of label {line[0]}")
+            raise Exception(f"LINE[{ self.main.lineno}]\tMULTI useing of label {line[0]}")      
         else:
             #nshof hya c walla x
             temp=line[2]
+            self.main.symtab[line[0].upper()]=self.main.current_loc
             if temp[0].upper() == 'C':
                 #convert chars to hex
                 hex=Number(temp[2:-1]).chars_to_hex()
@@ -41,11 +70,197 @@ class Directives:
                         True
                     )
                 )
-                self.main.symtab[line[0].upper()]=self.main.current_loc
+                self.main.current_loc = self.linef(Number(hex).int()*3,6)
+            elif temp[0].upper() == 'X':
+                if Number(temp[2:-1]).test_hex() == False:
+                    raise Exception(f"LINE[{self.main.lineno}]\t starting address must to be hex")
+                else:
+                    self.main.Lines.append(
+                        l(
+                            line,
+                            self.main.lineno,
+                            self.main.current_loc,
+                            "BYTE",
+                            True
+                        )
+                    )
+                    self.main.current_loc = self.linef(Number(temp[2:-1]).int()*3,6)
+            elif not Number(temp).test_hex: #int
+                    self.main.Lines.append(
+                        l(
+                            line,
+                            self.main.lineno,
+                            self.main.current_loc,
+                            "BYTE",
+                            True
+                        )
+                    )
+                    self.main.current_loc = self.linef(int(temp[2:-1])*3,6)               
+            else:
+                raise Exception(f"LINE[{ self.main.lineno}]\tformate {temp[0]} is undefined")    
+
+
+    def RESB(self,line):
+        if "RESB" == line[0].upper():
+            raise Exception(f"LINE[{ self.main.lineno}]\tthe name of LABEL can't to set as RESB")
+        elif len(line) > 3:
+            raise Exception(f"LINE[{ self.main.lineno}]\tWrong RESB formate")  
+        elif  [i.upper() for i in line].count("RESB") > 1:
+            raise Exception(f"LINE[{ self.main.lineno}]\tFalse [Multiple] RESB position")
+        elif line[0].upper() in self.main.symtab:
+            raise Exception(f"LINE[{ self.main.lineno}]\tMULTI useing of label {line[0]}")      
+        else:
+            #nshof hya c walla x
+            temp=line[2]
+            self.main.symtab[line[0].upper()]=self.main.current_loc
+            if temp[0].upper() == 'C':
+                #convert chars to hex
+                hex=Number(temp[2:-1]).chars_to_hex()
+                self.main.Lines.append(
+                    l(
+                        line,
+                        self.main.lineno,
+                        self.main.current_loc,
+                        "RESB",
+                        True
+                    )
+                )
+                self.main.current_loc = self.linef(Number(hex).int(),6)
+            elif temp[0].upper() == 'X':
+                if Number(temp[2:-1]).test_hex() == False:
+                    raise Exception(f"LINE[{self.main.lineno}]\t starting address must to be hex")
+                else:
+                    self.main.Lines.append(
+                        l(
+                            line,
+                            self.main.lineno,
+                            self.main.current_loc,
+                            "BYTE",
+                            True
+                        )
+                    )
+                    self.main.current_loc = self.linef(Number(temp[2:-1]).int(),6)
+            elif not Number(temp).test_hex: #int
+                    self.main.Lines.append(
+                        l(
+                            line,
+                            self.main.lineno,
+                            self.main.current_loc,
+                            "BYTE",
+                            True
+                        )
+                    )
+                    self.main.current_loc = self.linef(int(temp[2:-1]),6)               
+            else:
+                raise Exception(f"LINE[{ self.main.lineno}]\tformate {temp[0]} is undefined")    
+
+
+
+    def word(self,line):
+        if "WORD" == line[0].upper():
+            raise Exception(f"LINE[{ self.main.lineno}]\tthe name of LABEL can't to set as WORD")
+        elif len(line) > 3:
+            raise Exception(f"LINE[{ self.main.lineno}]\tWrong WORD formate")  
+        elif  [i.upper() for i in line].count("WORD") > 1:
+            raise Exception(f"LINE[{ self.main.lineno}]\tFalse [Multiple] WORD position")
+        elif line[0].upper() in self.main.symtab:
+            raise Exception(f"LINE[{ self.main.lineno}]\tMULTI useing of label {line[0]}")      
+        else:
+            #nshof hya c walla x
+            temp=line[2]
+            self.main.symtab[line[0].upper()]=self.main.current_loc
+            if temp[0].upper() == 'C':
+                #convert chars to hex
+                hex=Number(temp[2:-1]).chars_to_hex()
+                self.main.Lines.append(
+                    l(
+                        line,
+                        self.main.lineno,
+                        self.main.current_loc,
+                        "BYTE",
+                        True
+                    )
+                )
+                self.main.current_loc = self.linef(3,6)
+            elif temp[0].upper() == 'X':
+                if Number(temp[2:-1]).test_hex() == False:
+                    raise Exception(f"LINE[{self.main.lineno}]\t starting address must to be hex")
+                else:
+                    self.main.Lines.append(
+                        l(
+                            line,
+                            self.main.lineno,
+                            self.main.current_loc,
+                            "BYTE",
+                            True
+                        )
+                    )
+                    self.main.current_loc = self.linef(3,6)
+            elif not Number(temp).test_hex: #int
+                    self.main.Lines.append(
+                        l(
+                            line,
+                            self.main.lineno,
+                            self.main.current_loc,
+                            "BYTE",
+                            True
+                        )
+                    )
+                    self.main.current_loc = self.linef(3,6)               
+            else:
+                raise Exception(f"LINE[{ self.main.lineno}]\tformate {temp[0]} is undefined")             
+
+    def byte(self,line):
+        if "BYTE" == line[0].upper():
+            raise Exception(f"LINE[{ self.main.lineno}]\tthe name of LABEL can't to set as BYTE")
+        elif len(line) > 3:
+            raise Exception(f"LINE[{ self.main.lineno}]\tWrong BYTE formate")  
+        elif  [i.upper() for i in line].count("BYTE") > 1:
+            raise Exception(f"LINE[{ self.main.lineno}]\tFalse [Multiple] BYTE position")
+        elif line[0].upper() in self.main.symtab:
+            raise Exception(f"LINE[{ self.main.lineno}]\tMULTI useing of label {line[0]}")
+        else:
+            #nshof hya c walla x
+            temp=line[2]
+            self.main.symtab[line[0].upper()]=self.main.current_loc
+            if temp[0].upper() == 'C':
+                #convert chars to hex
+                hex=Number(temp[2:-1]).chars_to_hex()
+                self.main.Lines.append(
+                    l(
+                        line,
+                        self.main.lineno,
+                        self.main.current_loc,
+                        "BYTE",
+                        True
+                    )
+                )
                 self.main.current_loc = self.linef(len(temp[2:-1]),6)
             elif temp[0].upper() == 'X':
                 if Number(temp[2:-1]).test_hex() == False:
                     raise Exception(f"LINE[{self.main.lineno}]\t starting address must to be hex")
+                else:
+                    self.main.Lines.append(
+                        l(
+                            line,
+                            self.main.lineno,
+                            self.main.current_loc,
+                            "BYTE",
+                            True
+                        )
+                    )
+                    self.main.current_loc = self.linef(ceil(len(temp[2:-1])/2),6)
+            elif not Number(temp).test_hex: #int
+                    self.main.Lines.append(
+                        l(
+                            line,
+                            self.main.lineno,
+                            self.main.current_loc,
+                            "BYTE",
+                            True
+                        )
+                    )
+                    self.main.current_loc = self.linef(ceil(len(Number(temp).hex())/2),6)               
             else:
                 raise Exception(f"LINE[{ self.main.lineno}]\tformate {temp[0]} is undefined")
 
@@ -86,11 +301,10 @@ class Directives:
                     l(
                         line,
                         self.main.lineno,
-                        self.main.current_loc,
+                        None,
                         "START",
                         True
                     )
                 )
                 self.main.current_loc = self.linef(4,6)
-                print( self.main.current_loc)
 
